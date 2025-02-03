a9b40eac13be46908ff799cfbeddd3d200fa982e
Ilya Usov <ilya671qw@gmail.com>
2025-02-03 13:04:50 +0300

    Changed sanitizing filters

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ require('model/assignments_db.php');
 require('model/course_db.php');
 
 $assignment_id = filter_input(INPUT_POST, 'assignment_id', FILTER_VALIDATE_INT);
-$description = filter_input(INPUT_POST, 'description', FILTER_UNSAFE_RAW);
-$course_name = filter_input(INPUT_POST, 'course_name', FILTER_UNSAFE_RAW);
+$description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+$course_name = filter_input(INPUT_POST, 'course_name', FILTER_SANITIZE_SPECIAL_CHARS);
 $course_id = filter_input(INPUT_POST, 'course_id', FILTER_VALIDATE_INT);
 
 if (!$course_id) {
     $course_id = filter_input(INPUT_GET, 'course_id', FILTER_VALIDATE_INT);
 }
 
-$action = filter_input(INPUT_POST, 'action', FILTER_UNSAFE_RAW);
+$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
 
 if (!$action) {
-    $action = filter_input(INPUT_GET, 'action', FILTER_UNSAFE_RAW);
+    $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
     if (!$action) {
         $action = 'list_assignments';
     }
